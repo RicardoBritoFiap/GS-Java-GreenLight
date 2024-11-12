4adf77df7fcb217fd8135be35e30dc5107852600
pedro <pedrosbmartinazzo@hotmail.com>
2024-11-12 00:10:45 -0300

    projeto base

---
+++ src/main/java/com/pedrosbm/GreenLight/GreenLightApplication.java
@@ -0,0 +1,13 @@
+package com.pedrosbm.GreenLight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenLightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenLightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrosbm/GreenLight/GreenLightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrosbm.GreenLight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenLightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
